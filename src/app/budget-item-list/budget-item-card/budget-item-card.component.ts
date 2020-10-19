42bb682bc28bf40aa8d1e34ff89b69bc9d02f86d
John Grant <john.grant94@hotmail.com>
2020-10-18 23:39:36 -0400

    first commit

---
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+
+@Component({
+  selector: 'app-budget-item-card',
+  templateUrl: './budget-item-card.component.html',
+  styleUrls: ['./budget-item-card.component.scss']
+})
+export class BudgetItemCardComponent implements OnInit {
+
+  @Input() item: BudgetItem;
+  @Output() xButtonClick: EventEmitter<void> = new EventEmitter<void>();
+  @Output() cardClicked: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onXButtonClick() {
+    this.xButtonClick.emit();
+  }
+
+  onCardClick() {
+    this.cardClicked.emit();
+  }
+
+}
